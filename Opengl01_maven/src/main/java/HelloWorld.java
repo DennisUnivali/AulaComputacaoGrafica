7ad7b3fbbc4a8565dab9b007396c8178fb77c6f9
dennis <dennis@palmsoft.com.br>
2024-04-23 21:03:45 -0300

    Versão Inicial

--- Opengl01_maven/src/main/java/HelloWorld.java
+++ Opengl01_maven/src/main/java/HelloWorld.java
@@ -17,15 +17,22 @@ import static org.lwjgl.system.MemoryUtil.*;
 public class HelloWorld {
 
 	// The window handle
-	private static long window;
+	private long window;
 
-//	public void run() {
-//		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
-//
-//		loop();
-//
-//
-//	}
+	public void run() {
+		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+		init();
+		loop();
+
+		// Free the window callbacks and destroy the window
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+
+		// Terminate GLFW and free the error callback
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
 
 	private void init() {
 		// Setup an error callback. The default implementation
@@ -155,17 +162,7 @@ public class HelloWorld {
 	}
 
 	public static void main(String[] args) {
-		HelloWorld hw = new HelloWorld();
-		hw.init();
-		hw.loop();
-		
-		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
+		new HelloWorld().run();
 	}
 	
 	public static void gluPerspective(float fovy, float aspect, float near, float far) {
